765b1311f1d44106ad371c3bf3ec0ec93f92abde
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-10 15:14:58 +0300

    src/main/webapp/WEB-INF/jsp/help.html.jsp
    fix help page to load correctly

---
+++ src/main/java/eu/uberdust/rest/Main.java
@@ -0,0 +1,11 @@
+package eu.uberdust.rest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amaxilatis
+ * Date: 7/8/13
+ * Time: 2:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
