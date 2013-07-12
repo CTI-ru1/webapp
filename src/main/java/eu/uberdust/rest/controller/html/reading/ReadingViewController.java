0b2f41d059720fc379b6e7e1d8cd32e5e9f31835
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-12 12:48:18 +0300

    merge and update readings controller

--- src/main/java/eu/uberdust/rest/controller/html/reading/ReadingViewController.java
+++ src/main/java/eu/uberdust/rest/controller/html/reading/ReadingViewController.java
@@ -1,7 +1,6 @@
 package eu.uberdust.rest.controller.html.reading;
 
 import eu.uberdust.caching.Loggable;
-import eu.uberdust.command.NodeCapabilityCommand;
 import eu.uberdust.formatter.HtmlFormatter;
 import eu.uberdust.rest.exception.*;
 import eu.wisebed.wisedb.controller.CapabilityController;
@@ -13,12 +12,12 @@ import eu.wisebed.wisedb.model.Node;
 import eu.wisebed.wisedb.model.NodeReading;
 import eu.wisebed.wisedb.model.Testbed;
 import org.apache.log4j.Logger;
-import org.springframework.validation.BindException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractRestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +25,14 @@ import java.util.Map;
 /**
  * Controller class that returns an HTML page containing a list of the readings for a node/capability.
  */
-public final class ShowByDateController extends AbstractRestController {
+@Controller
+@RequestMapping("/testbed/{testbedId}/node/{nodeName}/capability/{capabilityName}/html")
+public final class ReadingViewController {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(ReadingViewController.class);
 
     /**
      * Node peristence manager.
@@ -49,25 +55,11 @@ public final class ShowByDateController extends AbstractRestController {
     private transient TestbedController testbedManager;
 
     /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(ShowByDateController.class);
-
-    /**
-     * Constructor.
-     */
-    public ShowByDateController() {
-        super();
-
-        // Make sure to set which method this controller will support.
-        this.setSupportedMethods(new String[]{METHOD_GET});
-    }
-
-    /**
      * Sets node persistence manager.
      *
      * @param nodeManager node persistence manager.
      */
+    @Autowired
     public void setNodeManager(final NodeController nodeManager) {
         this.nodeManager = nodeManager;
     }
@@ -77,6 +69,7 @@ public final class ShowByDateController extends AbstractRestController {
      *
      * @param capabilityManager capability persistence manager.
      */
+    @Autowired
     public void setCapabilityManager(final CapabilityController capabilityManager) {
         this.capabilityManager = capabilityManager;
     }
@@ -86,6 +79,7 @@ public final class ShowByDateController extends AbstractRestController {
      *
      * @param nodeReadingManager NodeReading persistence manager.
      */
+    @Autowired
     public void setNodeReadingManager(final NodeReadingController nodeReadingManager) {
         this.nodeReadingManager = nodeReadingManager;
     }
@@ -95,6 +89,7 @@ public final class ShowByDateController extends AbstractRestController {
      *
      * @param testbedManager Testbed persistence manager.
      */
+    @Autowired
     public void setTestbedManager(final TestbedController testbedManager) {
         this.testbedManager = testbedManager;
     }
@@ -102,10 +97,64 @@ public final class ShowByDateController extends AbstractRestController {
     /**
      * Handle Request and return the appropriate response.
      *
-     * @param req        http servlet req.
-     * @param response   http servlet response.
-     * @param commandObj command object.
-     * @param errors     BindException exception.
+     * @return response http servlet response.
+     * @throws InvalidNodeIdException         invalid node id exception.
+     * @throws InvalidCapabilityNameException invalid capability name exception.
+     * @throws InvalidTestbedIdException      invalid testbed id exception.
+     * @throws TestbedNotFoundException       testbed not found exception.
+     * @throws NodeNotFoundException          node not found exception.
+     * @throws CapabilityNotFoundException    capability not found exception.
+     * @throws InvalidLimitException          invalid limit exception.
+     */
+    @Loggable
+    @RequestMapping("/limit/{limit}")
+    public ModelAndView showReadings(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, @PathVariable("limit") int limit)
+            throws CapabilityNotFoundException, NodeNotFoundException, TestbedNotFoundException,
+            InvalidTestbedIdException, InvalidCapabilityNameException, InvalidNodeIdException, InvalidLimitException {
+
+        final long start = System.currentTimeMillis();
+
+        // look up testbed
+        final Testbed testbed = testbedManager.getByID(testbedId);
+        if (testbed == null) {
+            // if no testbed is found throw exception
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].");
+        }
+
+        // retrieve node
+        final Node node = nodeManager.getByName(nodeName);
+        if (node == null) {
+            throw new NodeNotFoundException("Cannot find node [" + nodeName + "]");
+        }
+
+        // retrieve capability
+        final Capability capability = capabilityManager.getByID(capabilityName);
+        if (capability == null) {
+            throw new CapabilityNotFoundException("Cannot find capability [" + capabilityName + "]");
+        }
+
+        // retrieve readings based on node/capability
+        final List<NodeReading> nodeReadings;
+        nodeReadings = nodeReadingManager.listNodeReadings(node, capability, limit);
+
+        // Prepare data to pass to jsp
+        final Map<String, Object> refData = new HashMap<String, Object>();
+
+        // else put thisNode instance in refData and return index view
+        refData.put("testbedId", testbedId);
+        refData.put("readings", nodeReadings);
+        refData.put("testbed", testbed);
+        refData.put("capability", capability);
+        refData.put("node", node);
+
+        refData.put("time", String.valueOf((System.currentTimeMillis() - start)));
+        // check type of view requested
+        return new ModelAndView("nodecapability/readings.html", refData);
+    }
+
+    /**
+     * Handle Request and return the appropriate response.
+     *
      * @return response http servlet response.
      * @throws eu.uberdust.rest.exception.InvalidNodeIdException
      *          invalid node id exception.
@@ -123,60 +172,35 @@ public final class ShowByDateController extends AbstractRestController {
      *          invalid limit exception.
      */
     @Loggable
-    protected ModelAndView handle(final HttpServletRequest req, final HttpServletResponse response,
-                                  final Object commandObj, final BindException errors)
+    @RequestMapping("/from/{from}/to/{to}/")
+    public ModelAndView showReadingsByDate(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, @PathVariable("from") long from, @PathVariable("to") long to)
             throws CapabilityNotFoundException, NodeNotFoundException, TestbedNotFoundException,
             InvalidTestbedIdException, InvalidCapabilityNameException, InvalidNodeIdException, InvalidLimitException {
 
-        HtmlFormatter.getInstance().setBaseUrl(req.getRequestURL().substring(0, req.getRequestURL().indexOf("/rest")));
-
         final long start = System.currentTimeMillis();
 
-        // set commandNode object
-        final NodeCapabilityCommand command = (NodeCapabilityCommand) commandObj;
-
-        // check node id
-        if (command.getNodeId() == null || command.getNodeId().isEmpty()) {
-            throw new InvalidNodeIdException("Must provide node id");
-        }
-
-        // check capability name
-        if (command.getCapabilityId() == null || command.getCapabilityId().isEmpty()) {
-            throw new InvalidCapabilityNameException("Must provide capability name");
-        }
-
-        // a specific testbed is requested by testbed Id
-        int testbedId;
-        try {
-            testbedId = Integer.parseInt(command.getTestbedId());
-
-        } catch (NumberFormatException nfe) {
-            throw new InvalidTestbedIdException("Testbed IDs have number format.", nfe);
-        }
-
         // look up testbed
-        final Testbed testbed = testbedManager.getByID(Integer.parseInt(command.getTestbedId()));
+        final Testbed testbed = testbedManager.getByID(testbedId);
         if (testbed == null) {
             // if no testbed is found throw exception
             throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].");
         }
 
         // retrieve node
-        final Node node = nodeManager.getByName(command.getNodeId());
+        final Node node = nodeManager.getByName(nodeName);
         if (node == null) {
-            throw new NodeNotFoundException("Cannot find node [" + command.getNodeId() + "]");
+            throw new NodeNotFoundException("Cannot find node [" + nodeName + "]");
         }
 
         // retrieve capability
-        final Capability capability = capabilityManager.getByID(command.getCapabilityId());
+        final Capability capability = capabilityManager.getByID(capabilityName);
         if (capability == null) {
-            throw new CapabilityNotFoundException("Cannot find capability [" + command.getCapabilityId() + "]");
+            throw new CapabilityNotFoundException("Cannot find capability [" + capabilityName + "]");
         }
 
         // retrieve readings based on node/capability
         final List<NodeReading> nodeReadings;
-        final long from = command.getReadingsFrom() == null ? 0 : Long.parseLong(command.getReadingsFrom());
-        final long to = command.getReadingsTo() == null ? 0 : Long.parseLong(command.getReadingsTo());
+
 
         // no limit is provided
         nodeReadings = nodeReadingManager.listNodeReadings(node, capability, from, to);
@@ -185,17 +209,10 @@ public final class ShowByDateController extends AbstractRestController {
         final Map<String, Object> refData = new HashMap<String, Object>();
 
         // else put thisNode instance in refData and return index view
-        refData.put("testbedId", command.getTestbedId());
-        refData.put("text", HtmlFormatter.getInstance().
-
-                formatNodeReadings(nodeReadings)
-
-        );
-
+        refData.put("testbedId", testbedId);
+        refData.put("text", HtmlFormatter.getInstance().formatNodeReadings(nodeReadings));
         refData.put("time", String.valueOf((System.currentTimeMillis() - start)));
         // check type of view requested
-        return new
-
-                ModelAndView("nodecapability/readings.html", refData);
+        return new ModelAndView("nodecapability/readings.html", refData);
     }
 }
