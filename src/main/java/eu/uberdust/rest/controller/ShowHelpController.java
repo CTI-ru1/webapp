c413b6e9733b27fc0d1cceadfbb1b8148d2ebc7f
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-12 12:10:38 +0300

    update help controller

--- src/main/java/eu/uberdust/rest/controller/ShowHelpController.java
+++ src/main/java/eu/uberdust/rest/controller/ShowHelpController.java
@@ -4,9 +4,12 @@ import eu.uberdust.caching.Loggable;
 import eu.uberdust.formatter.HtmlFormatter;
 import eu.uberdust.rest.exception.*;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractRestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,53 +19,21 @@ import java.util.Map;
 /**
  * Controller class that returns an HTML page containing a list of the readings for a node/capability.
  */
-public final class ShowHelpController extends AbstractRestController {
+@Controller
+@RequestMapping("/help")
+public final class ShowHelpController {
 
     /**
      * Logger.
      */
     private static final Logger LOGGER = Logger.getLogger(ShowHelpController.class);
 
-    /**
-     * Constructor.
-     */
-    public ShowHelpController() {
-        super();
-
-        // Make sure to set which method this controller will support.
-        this.setSupportedMethods(new String[]{METHOD_GET});
-    }
-
-    /**
-     * Handle Request and return the appropriate response.
-     *
-     * @param req        http servlet req.
-     * @param response   http servlet response.
-     * @param commandObj command object.
-     * @param errors     BindException exception.
-     * @return response http servlet response.
-     * @throws eu.uberdust.rest.exception.InvalidNodeIdException
-     *          invalid node id exception.
-     * @throws eu.uberdust.rest.exception.InvalidCapabilityNameException
-     *          invalid capability name exception.
-     * @throws eu.uberdust.rest.exception.InvalidTestbedIdException
-     *          invalid testbed id exception.
-     * @throws eu.uberdust.rest.exception.TestbedNotFoundException
-     *          testbed not found exception.
-     * @throws eu.uberdust.rest.exception.NodeNotFoundException
-     *          node not found exception.
-     * @throws eu.uberdust.rest.exception.CapabilityNotFoundException
-     *          capability not found exception.
-     * @throws eu.uberdust.rest.exception.InvalidLimitException
-     *          invalid limit exception.
-     */
     @Loggable
-    protected ModelAndView handle(final HttpServletRequest req, final HttpServletResponse response,
-                                  final Object commandObj, final BindException errors)
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView handle()
             throws CapabilityNotFoundException, NodeNotFoundException, TestbedNotFoundException,
             InvalidTestbedIdException, InvalidCapabilityNameException, InvalidNodeIdException, InvalidLimitException {
 
-        HtmlFormatter.getInstance().setBaseUrl(req.getRequestURL().substring(0, req.getRequestURL().indexOf("/rest")));
 
         final long start = System.currentTimeMillis();
 
