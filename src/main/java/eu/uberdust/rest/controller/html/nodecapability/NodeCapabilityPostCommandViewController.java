0981d8ad9c508c633782c3ce117119fb166ac09a
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-12 12:58:16 +0300

    update and merge post controller

--- src/main/java/eu/uberdust/rest/controller/html/nodecapability/NodeCapabilityPostCommandViewController.java
+++ src/main/java/eu/uberdust/rest/controller/html/nodecapability/NodeCapabilityPostCommandViewController.java
@@ -5,7 +5,6 @@ import ch.ethz.inf.vs.californium.coap.Option;
 import ch.ethz.inf.vs.californium.coap.OptionNumberRegistry;
 import ch.ethz.inf.vs.californium.coap.Request;
 import eu.uberdust.caching.Loggable;
-import eu.uberdust.command.NodeCapabilityCommand;
 import eu.uberdust.rest.exception.*;
 import eu.uberdust.util.CommandDispatcher;
 import eu.wisebed.wisedb.controller.CapabilityController;
@@ -15,20 +14,28 @@ import eu.wisebed.wisedb.model.Capability;
 import eu.wisebed.wisedb.model.Node;
 import eu.wisebed.wisedb.model.Testbed;
 import org.apache.log4j.Logger;
-import org.springframework.validation.BindException;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractRestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Random;
 
 /**
  * Controller class that returns an HTML page containing a list of the readings for a node/capability.
  */
-public final class PostController extends AbstractRestController {
+@Controller
+@RequestMapping("/testbed/{testbedId}/node/{nodeName}/capability/{capabilityName}/{payload}/")
+public final class NodeCapabilityPostCommandViewController {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(NodeCapabilityPostCommandViewController.class);
 
     /**
      * Node peristence manager.
@@ -46,25 +53,11 @@ public final class PostController extends AbstractRestController {
     private transient TestbedController testbedManager;
 
     /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(PostController.class);
-
-    /**
-     * Constructor.
-     */
-    public PostController() {
-        super();
-
-        // Make sure to set which method this controller will support.
-        this.setSupportedMethods(new String[]{METHOD_POST});
-    }
-
-    /**
      * Sets node persistence manager.
      *
      * @param nodeManager node persistence manager.
      */
+    @Autowired
     public void setNodeManager(final NodeController nodeManager) {
         this.nodeManager = nodeManager;
     }
@@ -74,6 +67,7 @@ public final class PostController extends AbstractRestController {
      *
      * @param capabilityManager capability persistence manager.
      */
+    @Autowired
     public void setCapabilityManager(final CapabilityController capabilityManager) {
         this.capabilityManager = capabilityManager;
     }
@@ -83,6 +77,7 @@ public final class PostController extends AbstractRestController {
      *
      * @param testbedManager Testbed persistence manager.
      */
+    @Autowired
     public void setTestbedManager(final TestbedController testbedManager) {
         this.testbedManager = testbedManager;
     }
@@ -90,10 +85,6 @@ public final class PostController extends AbstractRestController {
     /**
      * Handle Request and return the appropriate response.
      *
-     * @param req        http servlet req.
-     * @param response   http servlet response.
-     * @param commandObj command object.
-     * @param errors     BindException exception.
      * @return response http servlet response.
      * @throws eu.uberdust.rest.exception.InvalidNodeIdException
      *          invalid node id exception.
@@ -111,51 +102,29 @@ public final class PostController extends AbstractRestController {
      *          invalid limit exception.
      */
     @Loggable
-    protected ModelAndView handle(final HttpServletRequest req, final HttpServletResponse response,
-                                  final Object commandObj, final BindException errors)
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public String handle(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, @PathVariable("payload") String payload, HttpServletResponse response)
             throws CapabilityNotFoundException, NodeNotFoundException, TestbedNotFoundException,
             InvalidTestbedIdException, InvalidCapabilityNameException, InvalidNodeIdException, InvalidLimitException, IOException {
 
-
-        // set commandNode object
-        final NodeCapabilityCommand command = (NodeCapabilityCommand) commandObj;
-
-        // check node id
-        if (command.getNodeId() == null || command.getNodeId().isEmpty()) {
-            throw new InvalidNodeIdException("Must provide node id");
-        }
-
-        // check capability name
-        if (command.getCapabilityId() == null || command.getCapabilityId().isEmpty()) {
-            throw new InvalidCapabilityNameException("Must provide capability name");
-        }
-
-        // a specific testbed is requested by testbed Id
-        int testbedId;
-        try {
-            testbedId = Integer.parseInt(command.getTestbedId());
-
-        } catch (NumberFormatException nfe) {
-            throw new InvalidTestbedIdException("Testbed IDs have number format.", nfe);
-        }
-
         // look up testbed
-        final Testbed testbed = testbedManager.getByID(Integer.parseInt(command.getTestbedId()));
+        final Testbed testbed = testbedManager.getByID(testbedId);
         if (testbed == null) {
             // if no testbed is found throw exception
             throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].");
         }
 
         // retrieve node
-        final Node node = nodeManager.getByName(command.getNodeId());
+        final Node node = nodeManager.getByName(nodeName);
         if (node == null) {
-            throw new NodeNotFoundException("Cannot find node [" + command.getNodeId() + "]");
+            throw new NodeNotFoundException("Cannot find node [" + nodeName + "]");
         }
 
         // retrieve capability
-        final Capability capability = capabilityManager.getByID(command.getCapabilityId());
+        final Capability capability = capabilityManager.getByID(capabilityName);
         if (capability == null) {
-            throw new CapabilityNotFoundException("Cannot find capability [" + command.getCapabilityId() + "]");
+            throw new CapabilityNotFoundException("Cannot find capability [" + capabilityName + "]");
         }
 
 
@@ -168,7 +137,7 @@ public final class PostController extends AbstractRestController {
         }
         final String capShortName = capability.getName().substring(capability.getName().lastIndexOf(":") + 1);
         coapReq.setURI(capShortName);
-        coapReq.setPayload(command.getStringReading());
+        coapReq.setPayload(payload);
 
         StringBuilder payloadStringBuilder = new StringBuilder();
         for (Byte data : coapReq.toByteArray()) {
@@ -181,11 +150,10 @@ public final class PostController extends AbstractRestController {
 
         CommandDispatcher.getInstance().sendCommand(node.getSetup().getId(), node.getName(), payloadString);
 
+        response.setStatus(200);
         response.setContentType("text/plain");
-        final Writer textOutput = (response.getWriter());
-        textOutput.write("OK . Destination : " + node.getName() + "\nPayload : " + payloadString);
-        coapReq.prettyPrint();
+        response.setCharacterEncoding("UTF-8");
 
-        return null;
+        return "OK . Destination : " + node.getName() + "\nPayload : " + payloadString;
     }
 }
