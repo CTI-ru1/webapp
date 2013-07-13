b97884ab702686db9329e7e3166c23e0c3f0e8e7
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-13 15:57:44 +0300

    updated send command controller

--- src/main/java/eu/uberdust/rest/controller/SendCommandController.java
+++ src/main/java/eu/uberdust/rest/controller/SendCommandController.java
@@ -7,9 +7,16 @@ import eu.uberdust.util.CommandDispatcher;
 import eu.wisebed.wisedb.controller.NodeController;
 import eu.wisebed.wisedb.model.Node;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractRestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,12 +26,9 @@ import java.io.Writer;
 /**
  * Controller class for sending a message with certain payload to a destination.
  */
-public final class SendCommandController extends AbstractRestController {
-
-    /**
-     * Node persistence manager.
-     */
-    private transient NodeController nodeManager;
+@Controller
+@RequestMapping("/sendCommand/destination/{destinationName}/payload/{payloadSTR}")
+public final class SendCommandController {
 
     /**
      * Logger.
@@ -32,20 +36,16 @@ public final class SendCommandController extends AbstractRestController {
     private static final Logger LOGGER = Logger.getLogger(SendCommandController.class);
 
     /**
-     * Constructor.
+     * Node persistence manager.
      */
-    public SendCommandController() {
-        super();
-
-        // Make sure to set which method this controller will support.
-        this.setSupportedMethods(new String[]{METHOD_GET});
-    }
+    private transient NodeController nodeManager;
 
     /**
      * Sets node persistence manager.
      *
      * @param nodeManager node persistence manager.
      */
+    @Autowired
     public void setNodeManager(final NodeController nodeManager) {
         this.nodeManager = nodeManager;
     }
@@ -53,38 +53,33 @@ public final class SendCommandController extends AbstractRestController {
     /**
      * Handle Request and return the appropriate response.
      *
-     * @param request    http servlet request.
-     * @param response   http servlet response.
-     * @param commandObj command object.
-     * @param errors     BindException exception.
      * @return http servlet response.
      * @throws NodeNotFoundException NodeNotFoundException exception.
      * @throws IOException           IOException exception.
      */
     @Loggable
-    protected ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
-                                  final Object commandObj, final BindException errors)
+    @RequestMapping(method = RequestMethod.GET)
+    protected ResponseEntity<String> handle(@PathVariable("destinationName") String destinationName, @PathVariable("payloadSTR") String payloadSTR)
             throws NodeNotFoundException, IOException {
 
         // set commandNode object
-        final DestinationPayloadCommand command = (DestinationPayloadCommand) commandObj;
-        final String payload = command.getPayload().replaceAll(",", "");
+
+        final String payload = payloadSTR.replaceAll(",", "");
         final String nodeId = payload.substring(3);
 
         // look for destination node
-        final Node destinationNode = nodeManager.getByName(command.getDestination());
+        final Node destinationNode = nodeManager.getByName(destinationName);
         if (destinationNode == null) {
-            throw new NodeNotFoundException("Destination Node [" + command.getDestination() + "] is not stored.");
+            throw new NodeNotFoundException("Destination Node [" + destinationName + "] is not stored.");
         }
 
 
+        CommandDispatcher.getInstance().sendCommand(destinationNode.getSetup().getId(), destinationName, payloadSTR);
 
-        CommandDispatcher.getInstance().sendCommand(destinationNode.getSetup().getId(), command.getDestination(), command.getPayload());
-
-        response.setContentType("text/plain");
-        final Writer textOutput = (response.getWriter());
-        textOutput.write("OK . Destination : " + command.getDestination() + "\nPayload : " + command.getPayload());
-        return null;
-
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
+        return new ResponseEntity<String>(
+                "OK . Destination : " + destinationName + "\nPayload : " + payloadSTR
+                , responseHeaders, HttpStatus.OK);
     }
 }
