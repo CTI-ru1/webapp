2b2e940c75939ce4aab1a309ae82d3f2fe670649
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-13 15:31:26 +0300

    update and merge insert node readings controllers

---
+++ src/main/java/eu/uberdust/rest/controller/insert/node/InsertNodeReadingsViewController.java
@@ -0,0 +1,186 @@
+package eu.uberdust.rest.controller.insert.node;
+
+import eu.uberdust.caching.Loggable;
+import eu.uberdust.rest.exception.InvalidTestbedIdException;
+import eu.uberdust.rest.exception.TestbedNotFoundException;
+import eu.wisebed.wisedb.controller.NodeReadingController;
+import eu.wisebed.wisedb.controller.TestbedController;
+import eu.wisebed.wisedb.exception.UnknownTestbedException;
+import eu.wisebed.wisedb.model.NodeReading;
+import eu.wisebed.wisedb.model.Testbed;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * Controller class for inserting readings for a node capability pair.
+ */
+@Controller
+@RequestMapping("/testbed/{testbedId}/node/{nodeName}/capability/{capabilityName}/insert/timestamp/{timestampLONG}")
+public final class InsertNodeReadingsViewController {
+
+    /**
+     * Looger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(InsertNodeReadingsViewController.class);
+
+    /**
+     * NodeReading persistence manager.
+     */
+    private transient NodeReadingController nodeReadingManager;
+
+    /**
+     * Testbed persistence manager.
+     */
+    private transient TestbedController testbedManager;
+
+    /**
+     * Sets NodeReading persistence manager.
+     *
+     * @param nodeReadingManager NodeReading persistence manager.
+     */
+    @Autowired
+    public void setNodeReadingManager(final NodeReadingController nodeReadingManager) {
+        this.nodeReadingManager = nodeReadingManager;
+    }
+
+    /**
+     * Sets Testbed persistence manager.
+     *
+     * @param testbedManager Testbed persistence manager.
+     */
+    @Autowired
+    public void setTestbedManager(final TestbedController testbedManager) {
+        this.testbedManager = testbedManager;
+    }
+
+    /**
+     * Handle Request and return the appropriate response.
+     *
+     * @return response http servlet response.
+     * @throws InvalidTestbedIdException invalid testbed id exception.
+     * @throws TestbedNotFoundException  testbed not found exception.
+     * @throws IOException               IO exception.
+     */
+    @Loggable
+    @RequestMapping(value = "/reading/{readingSTR}", method = RequestMethod.GET)
+    protected ResponseEntity<String> handle(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, @PathVariable("timestampLONG") long timestampLONG, @PathVariable("readingSTR") String readingSTR)
+            throws InvalidTestbedIdException, TestbedNotFoundException, IOException {
+        // look up testbed
+        final Testbed testbed = testbedManager.getByID(testbedId);
+        if (testbed == null) {
+            // if no testbed is found throw exception
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].");
+        }
+
+        // parse reading and timestamp
+        final Double reading = new Double(readingSTR);
+        final Date timestamp = new Date(timestampLONG);
+
+        // insert reading
+        NodeReading addedReading;
+        try {
+            addedReading = nodeReadingManager.insertReading(nodeName, capabilityName, reading, null, timestamp);
+        } catch (UnknownTestbedException e) {
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].", e);
+        }
+
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
+        return new ResponseEntity<String>(
+                "Inserted for Node(" + nodeName + ") Capability(" + capabilityName + ") : " + reading + ". OK"
+                , responseHeaders, HttpStatus.OK);
+
+    }
+
+
+    /**
+     * Handle Request and return the appropriate response.
+     *
+     * @return response http servlet response.
+     * @throws eu.uberdust.rest.exception.InvalidTestbedIdException
+     *                             invalid testbed id exception.
+     * @throws eu.uberdust.rest.exception.TestbedNotFoundException
+     *                             testbed not found exception.
+     * @throws java.io.IOException IO exception.
+     */
+    @Loggable
+    @RequestMapping(value = "/reading/{readingDOUBLE}/stringreading/{readingSTR}", method = RequestMethod.GET)
+    public ResponseEntity<String> handles(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, @PathVariable("timestampLONG") long timestampLONG, @PathVariable("readingDOUBLE") double readingDOUBLE, @PathVariable("readingSTR") String readingSTR) throws InvalidTestbedIdException, TestbedNotFoundException, IOException {
+
+
+        // look up testbed
+        final Testbed testbed = testbedManager.getByID(testbedId);
+        if (testbed == null) {
+            // if no testbed is found throw exception
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].");
+        }
+
+        // parse reading and timestamp
+        final Date timestamp = new Date(timestampLONG);
+        final String nodeId = nodeName;
+        final String capabilityId = capabilityName;
+
+        // insert reading
+        try {
+            nodeReadingManager.insertReading(nodeId, capabilityId, readingDOUBLE, readingSTR, timestamp);
+        } catch (UnknownTestbedException e) {
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].", e);
+        }
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
+        return new ResponseEntity<String>(
+                "Inserted for Node(" + nodeName + ") Capability(" + capabilityName + ") : [" + readingDOUBLE + "," + readingSTR + "]. OK"
+                , responseHeaders, HttpStatus.OK);
+    }
+
+    /**
+     * Handle Request and return the appropriate response.
+     *
+     * @return response http servlet response.
+     * @throws eu.uberdust.rest.exception.InvalidTestbedIdException
+     *                             invalid testbed id exception.
+     * @throws eu.uberdust.rest.exception.TestbedNotFoundException
+     *                             testbed not found exception.
+     * @throws java.io.IOException IO exception.
+     */
+    @Loggable
+    @RequestMapping(value = "/stringreading/{readingSTR}", method = RequestMethod.GET)
+    public ResponseEntity<String> handles(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, @PathVariable("timestampLONG") long timestampLONG, @PathVariable("readingSTR") String readingSTR) throws InvalidTestbedIdException, TestbedNotFoundException, IOException {
+
+        // look up testbed
+        final Testbed testbed = testbedManager.getByID(testbedId);
+        if (testbed == null) {
+            // if no testbed is found throw exception
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].");
+        }
+
+        // parse reading and timestamp
+        final Date timestamp = new Date(timestampLONG);
+
+        // insert reading
+        try {
+            nodeReadingManager.insertReading(nodeName, capabilityName, null, readingSTR, timestamp);
+        } catch (UnknownTestbedException e) {
+            throw new TestbedNotFoundException("Cannot find testbed [" + testbedId + "].", e);
+        }
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
+        return new ResponseEntity<String>(
+                "Inserted for Node(" + nodeName + ") Capability(" + capabilityName
+                        + ") : " + readingSTR + ". OK"
+                , responseHeaders, HttpStatus.OK);
+    }
+}
