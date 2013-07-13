b5f2187397c309c7b5ef4144b89f8f92ebadc10d
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-13 15:20:07 +0300

    update live node readings controller

--- src/main/java/eu/uberdust/rest/controller/LiveNodeCapabilityController.java
+++ src/main/java/eu/uberdust/rest/controller/LiveNodeCapabilityController.java
@@ -10,9 +10,12 @@ import eu.uberdust.rest.exception.InvalidTestbedIdException;
 import eu.uberdust.rest.exception.NodeNotFoundException;
 import eu.uberdust.rest.exception.TestbedNotFoundException;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractRestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,29 +25,17 @@ import java.util.Map;
 /**
  * Controller class that returns an HTML page containing a list of the readings for a node/capability.
  */
-public final class LiveNodeCapabilityController extends AbstractRestController {
+@Controller
+@RequestMapping("/testbed/{testbedId}/node/{nodeName}/capability/{capabilityName}/live")
+public final class LiveNodeCapabilityController {
     /**
      * Logger.
      */
     private static final Logger LOGGER = Logger.getLogger(LiveNodeCapabilityController.class);
 
     /**
-     * Constructor.
-     */
-    public LiveNodeCapabilityController() {
-        super();
-
-        // Make sure to set which method this controller will support.
-        this.setSupportedMethods(new String[]{METHOD_GET});
-    }
-
-    /**
      * Handle Request and return the appropriate response.
      *
-     * @param req        http servlet req.
-     * @param response   http servlet response.
-     * @param commandObj command object.
-     * @param errors     BindException exception.
      * @return response http servlet response.
      * @throws eu.uberdust.rest.exception.InvalidNodeIdException
      *          invalid node id exception.
@@ -62,27 +53,24 @@ public final class LiveNodeCapabilityController extends AbstractRestController {
      *          invalid limit exception.
      */
     @Loggable
-    protected ModelAndView handle(final HttpServletRequest req, final HttpServletResponse response,
-                                  final Object commandObj, final BindException errors)
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView handle(@PathVariable("testbedId") int testbedId, @PathVariable("nodeName") String nodeName, @PathVariable("capabilityName") String capabilityName, HttpServletRequest request)
             throws CapabilityNotFoundException, NodeNotFoundException, TestbedNotFoundException,
             InvalidTestbedIdException, InvalidCapabilityNameException, InvalidNodeIdException, InvalidLimitException {
 
-        String hostname = req.getRequestURL().substring(0, req.getRequestURL().indexOf("/rest"));
+        String hostname = request.getRequestURL().substring(0, request.getRequestURL().indexOf("/rest"));
         hostname = hostname.replace("http://", "");
 
         final long start = System.currentTimeMillis();
-
-        // set commandNode object
-        final NodeCapabilityCommand command = (NodeCapabilityCommand) commandObj;
 
         // Prepare data to pass to jsp
         final Map<String, Object> refData = new HashMap<String, Object>();
 
         // else put thisNode instance in refData and return index view
-        refData.put("testbedId", command.getTestbedId());
+        refData.put("testbedId", testbedId);
         refData.put("host", hostname);
-        refData.put("node", command.getNodeId());
-        refData.put("capability", command.getCapabilityId());
+        refData.put("node", nodeName);
+        refData.put("capability", capabilityName);
         refData.put("time", String.valueOf((System.currentTimeMillis() - start)));
         // check type of view requested
         return new ModelAndView("nodecapability/live.html", refData);
