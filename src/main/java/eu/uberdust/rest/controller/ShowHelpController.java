e282f685ef932ed143fd3678e67a7b5218d3b0a0
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-15 15:30:43 +0300

    update properties for show help

--- src/main/java/eu/uberdust/rest/controller/ShowHelpController.java
+++ src/main/java/eu/uberdust/rest/controller/ShowHelpController.java
@@ -1,18 +1,13 @@
 package eu.uberdust.rest.controller;
 
 import eu.uberdust.caching.Loggable;
-import eu.uberdust.formatter.HtmlFormatter;
 import eu.uberdust.rest.exception.*;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +17,6 @@ import java.util.Map;
 @Controller
 @RequestMapping("/help")
 public final class ShowHelpController {
-
     /**
      * Logger.
      */
@@ -30,17 +24,14 @@ public final class ShowHelpController {
 
     @Loggable
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView handle()
+    public ModelAndView getHelp()
             throws CapabilityNotFoundException, NodeNotFoundException, TestbedNotFoundException,
             InvalidTestbedIdException, InvalidCapabilityNameException, InvalidNodeIdException, InvalidLimitException {
-
-
         final long start = System.currentTimeMillis();
 
         // Prepare data to pass to jsp
         final Map<String, Object> refData = new HashMap<String, Object>();
-
-        // check type of view requested
+        refData.put("time", String.valueOf((System.currentTimeMillis() - start)));
         return new ModelAndView("help.html", refData);
     }
 }
